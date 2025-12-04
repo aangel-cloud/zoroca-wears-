afab6cac62c085403703f46cfc5e8cfd240a2001
aangel-cloud <diamonddamilolaangel@gmail.com>
2025-12-04 09:34:47 +0100

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,18 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+e.preventDefault();
+
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("message").value;
+
+
+if (name === "" || email === "" || message === "") {
+alert("Please fill all fields");
+return;
+}
+
+
+alert("Thank you " + name + "! Your message has been sent to ZOROCA WEARS");
+this.reset();
+});
